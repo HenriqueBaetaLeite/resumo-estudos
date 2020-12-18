64768065bf0884f1256b43318a32b45970116328
henrique <hbaetaleite@gmail.com>
2020-12-17 22:11:48 -0300

    finalizado Bloco7.1

---
+++ Bloco6.1/script.js
@@ -0,0 +1,65 @@
+// Vamos praticar um pouco, criando uma tela de login? 🔥
+// Adicione os campos de entrada para email e senha.
+// Adicione um checkbox com o texto Lembre-me .
+// Adicione um botão com o texto Entrar .
+// Adicione os placeholder Digite seu email e Digite sua senha para os campos de email e senha.
+// Adicione o atributo required para os campos de email e senha.
+// Adicione o atributo autocomplete="off" para o campo de email.
+// Adicione o atributo autofocus para o campo de email.
+
+// 
+
+// EXERCÍCIOS
+
+// Criando um formulário de currículo.
+// Crie um arquivo HTML chamado form.html para o formulário.
+// Caso julgue necessário, crie estilos CSS para o seu formulário, de acordo com a sua imaginação.
+// Coloque-os em um arquivo styles.css .
+// Crie também um arquivo script.js para seu código JavaScript .
+// Vamos criar um formulário de cadastro de currículo com base na especificação a seguir:
+// Crie um <fieldset> para os seguintes dados pessoais:
+// Nome - Texto
+// Limite de 40 caracteres
+// Campo obrigatório
+// Email - Texto
+// Limite de 50 caracteres
+// Campo obrigatório
+// CPF - Texto
+// Limite de 11 caracteres
+// Campo obrigatório
+// Endereço - Texto
+// Limite de 200 caracteres
+// Campo obrigatório
+// Cidade - Texto
+// Limite de 28 caracteres
+// Campo obrigatório
+// Estado - ComboBox
+// Todos os estados do Brasil
+// Utilize estruturas de repetição via JavaScript para gerar os <option>
+// Campo obrigatório
+// Tipo - Radio Button
+// Casa, Apartamento
+// Campo obrigatório
+// Crie outro <fieldset> para dados do seu último emprego
+// Resumo do currículo - TextArea
+// Limite de 1000 caracteres
+// Campo obrigatório
+// Cargo - Texto
+// Limite de 40 caracteres
+// Campo obrigatório
+// Descrição do cargo - Texto
+// Limite de 500 caracteres
+// Campo obrigatório
+// Data de início - Texto
+// Verificar o formato da data dd/mm/aaaa .
+// O dia deve ser > 0 e <= 31.
+// O mês deve ser > 0 e <= 12.
+// O ano não pode ser negativo.
+// Campo obrigatório
+// Caso alguma das condições for inválida no momento do envio do formulário, exibir mensagem
+// de erro contextualizada.
+// Logo abaixo do formulário, crie um botão que:
+// Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault() .
+// Execute as validações que foram pedidas ao longo da montagem do formulário.
+// Monte uma <div> com o consolidado dos dados que foram inseridos no formulário.
+// Crie um botão Limpar que limpa todos os campos do formulário e a <div> com seu currículo também.
